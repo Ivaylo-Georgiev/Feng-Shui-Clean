636c02d0fc4afeb43f1daedf7ab378f3503b1194
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-05 18:18:54 +0300

    specify astrology allies

--- src/main/java/fmi/feng/shui/command/signs/AnimalSign.java
+++ src/main/java/fmi/feng/shui/command/signs/AnimalSign.java
@@ -2,20 +2,25 @@ package fmi.feng.shui.command.signs;
 
 public enum AnimalSign {
 
-	RAT(4, 23, 1, "OX"), OX(5, 1, 3, "RAT"), TIGER(6, 3, 5, "PIG"), RABBIT(7, 5, 7, "DOG"), DRAGON(8, 7, 9, "ROOSTER"),
-	SNAKE(9, 9, 11, "MONKEY"), HORSE(10, 11, 13, "SHEEP"), SHEEP(11, 13, 15, "HORSE"), MONKEY(0, 15, 17, "SNAKE"),
-	ROOSTER(1, 17, 19, "DRAGON"), DOG(2, 19, 21, "RABBIT"), PIG(3, 21, 23, "TIGER");
+	RAT(4, 23, 1, "OX", "DRAGON,MONKEY"), OX(5, 1, 3, "RAT", "SNAKE,ROOSTER"), TIGER(6, 3, 5, "PIG", "HORSE,DOG"),
+	RABBIT(7, 5, 7, "DOG", "SHEEP,PIG"), DRAGON(8, 7, 9, "ROOSTER", "MONKEY,RAT"),
+	SNAKE(9, 9, 11, "MONKEY", "OX,ROOSTER"), HORSE(10, 11, 13, "SHEEP", "TIGER,DOG"),
+	SHEEP(11, 13, 15, "HORSE", "RABBIT,PIG"), MONKEY(0, 15, 17, "SNAKE", "DRAGON,RAT"),
+	ROOSTER(1, 17, 19, "DRAGON", "OX,SNAKE"), DOG(2, 19, 21, "RABBIT", "TIGER,HORSE"),
+	PIG(3, 21, 23, "TIGER", "RABBIT,SHEEP");
 
 	private int yearModulo;
 	private int fromHour;
 	private int toHour;
 	private String secretFriend;
+	private String astrologyAllies;
 
-	private AnimalSign(int yearModulo, int fromHour, int toHour, String secretFriend) {
+	private AnimalSign(int yearModulo, int fromHour, int toHour, String secretFriend, String astrologyAllies) {
 		this.yearModulo = yearModulo;
 		this.fromHour = fromHour;
 		this.toHour = toHour;
 		this.secretFriend = secretFriend;
+		this.astrologyAllies = astrologyAllies;
 	}
 
 	public static AnimalSign mapYearToSign(int year) {
@@ -43,6 +48,10 @@ public enum AnimalSign {
 
 	public AnimalSign getSecretFriend() {
 		return valueOf(this.secretFriend);
+	}
+
+	public String getAstrologyAllies() {
+		return this.astrologyAllies;
 	}
 
 }
