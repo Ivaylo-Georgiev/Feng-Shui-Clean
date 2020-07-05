9d99bf688102574ddae514c8d320a06ea5785275
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-05 12:01:30 +0300

    remove duplicate code from OneParameterTemplateTest.java

--- src/test/java/fmi/fenf/shui/command/validation/templates/OneParameterTemplateTest.java
+++ src/test/java/fmi/fenf/shui/command/validation/templates/OneParameterTemplateTest.java
@@ -14,22 +14,25 @@ public class OneParameterTemplateTest {
 
 	private final static String PARAMETER = "parameter";
 
+	private OneParameterTemplate template;
+
+	public OneParameterTemplateTest() {
+		this.template = new OneParameterTemplate();
+	}
+
 	@Test
 	public void test_should_pass_one_parameter_and_validate_successfully() throws InvalidParametersCountException {
-		OneParameterTemplate template = new OneParameterTemplate();
 		boolean isValid = template.validateParametersCount(Arrays.asList(PARAMETER));
 		assertTrue(isValid);
 	}
 
 	@Test(expected = InvalidParametersCountException.class)
 	public void test_should_pass_zero_parameters_and_validate_unsuccessfully() throws InvalidParametersCountException {
-		OneParameterTemplate template = new OneParameterTemplate();
 		template.validateParametersCount(Collections.emptyList());
 	}
 
 	@Test(expected = InvalidParametersCountException.class)
 	public void test_should_pass_two_parameters_and_validate_unsuccessfully() throws InvalidParametersCountException {
-		OneParameterTemplate template = new OneParameterTemplate();
 		template.validateParametersCount(Arrays.asList(PARAMETER, PARAMETER));
 	}
 
