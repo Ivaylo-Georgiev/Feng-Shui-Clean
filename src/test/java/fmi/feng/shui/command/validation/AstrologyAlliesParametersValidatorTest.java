8300906b5978306315bbc3ad610bd227ff8a2ff1
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-05 18:20:22 +0300

    create AstrologyAlliesParametersValidatorTest.java

---
+++ src/test/java/fmi/feng/shui/command/validation/AstrologyAlliesParametersValidatorTest.java
@@ -0,0 +1,53 @@
+package fmi.feng.shui.command.validation;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import fmi.feng.shui.command.exceptions.InvalidParameterException;
+import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
+
+public class AstrologyAlliesParametersValidatorTest {
+
+	private final static String VALID_PARAMETER = "2020";
+	private final static String INVALID_TEXT_PARAMETER = "abcd";
+	private final static String INVALID_NEGATIVE_INTEGER_PARAMETER = "-1";
+
+	@Test
+	public void test_should_verify_valid_parameters_count_validation() throws InvalidParametersCountException {
+		AstrologyAlliesParametersValidator validator = new AstrologyAlliesParametersValidator(
+				Arrays.asList(VALID_PARAMETER));
+		assertTrue(validator.validateParametersCount());
+	}
+
+	@Test(expected = InvalidParametersCountException.class)
+	public void test_should_verify_invalid_parameters_count_validation() throws InvalidParametersCountException {
+		AstrologyAlliesParametersValidator validator = new AstrologyAlliesParametersValidator(
+				Arrays.asList(VALID_PARAMETER, VALID_PARAMETER));
+		validator.validateParametersCount();
+	}
+
+	@Test
+	public void test_should_verify_valid_parameter_validation() throws InvalidParameterException {
+		AstrologyAlliesParametersValidator validator = new AstrologyAlliesParametersValidator(
+				Arrays.asList(VALID_PARAMETER));
+		assertTrue(validator.validateParameters());
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void test_should_verify_invalid_text_parameter_validation() throws InvalidParameterException {
+		AstrologyAlliesParametersValidator validator = new AstrologyAlliesParametersValidator(
+				Arrays.asList(INVALID_TEXT_PARAMETER));
+		validator.validateParameters();
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void test_should_verify_invalid_negative_integer_parameter_validation() throws InvalidParameterException {
+		AstrologyAlliesParametersValidator validator = new AstrologyAlliesParametersValidator(
+				Arrays.asList(INVALID_NEGATIVE_INTEGER_PARAMETER));
+		validator.validateParameters();
+	}
+
+}
