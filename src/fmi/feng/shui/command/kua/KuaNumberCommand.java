9b13836834476212a2bbd7ffaf449238a0626a7b
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-05 11:24:11 +0300

    clean magic numbers in KUA number calculation

--- src/fmi/feng/shui/command/kua/KuaNumberCommand.java
+++ src/fmi/feng/shui/command/kua/KuaNumberCommand.java
@@ -4,6 +4,16 @@ import fmi.feng.shui.command.FengShuiCommand;
 
 public class KuaNumberCommand implements FengShuiCommand {
 
+	// Some schools of Feng Shui do not use the Kua Number 5
+	private final static int KUA_FIVE = 5;
+	// Females replace it with Kua 8
+	private final static int KUA_EIGHT = 8;
+	// Males replace it with Kua 2
+	private final static int KUA_TWO = 2;
+
+	private final static int FEMALE_KUA_ADDEND = 4;
+	private final static int MALE_KUA_MINUEND = 11;
+
 	private int year;
 	private Gender gender;
 
@@ -22,26 +32,28 @@ public class KuaNumberCommand implements FengShuiCommand {
 
 	private int calculateMaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.year);
-		int kuaNumber = 11 - reducedYear;
+		int kuaNumber = MALE_KUA_MINUEND - reducedYear;
 		kuaNumber = reduceToSingleDigit(kuaNumber);
-		return (kuaNumber == 5) ? 2 : kuaNumber;
+		return (kuaNumber == KUA_FIVE) ? KUA_TWO : kuaNumber;
 	}
 
 	private int calculateFemaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.year);
-		int kuaNumber = 4 + reducedYear;
+		int kuaNumber = FEMALE_KUA_ADDEND + reducedYear;
 		kuaNumber = reduceToSingleDigit(kuaNumber);
-		return (kuaNumber == 5) ? 8 : kuaNumber;
+		return (kuaNumber == KUA_FIVE) ? KUA_EIGHT : kuaNumber;
 	}
 
 	private int reduceToSingleDigit(int number) {
 		int sum = 0;
 
+		// iterate until the number is reduced to 1 digit
 		while (number > 0 || sum > 9) {
 			if (number == 0) {
 				number = sum;
 				sum = 0;
 			}
+			// remove last digit
 			sum += number % 10;
 			number /= 10;
 		}
