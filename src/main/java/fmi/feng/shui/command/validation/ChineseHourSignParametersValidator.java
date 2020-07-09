d786a69d17c15f1657e685ca8fa94e88838b2060
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-09 18:33:40 +0300

    use newly created helper for Chinese hour sign hour validation

--- src/main/java/fmi/feng/shui/command/validation/ChineseHourSignParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/ChineseHourSignParametersValidator.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import fmi.feng.shui.command.exceptions.InvalidParameterException;
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
+import fmi.feng.shui.command.validation.helpers.HourParameterValidationHelper;
 import fmi.feng.shui.command.validation.helpers.OneParameterValidationHelper;
 
 public class ChineseHourSignParametersValidator extends CommandParametersValidator {
 
 	private final static int PARAMATER_INDEX = 0;
-	private final static int MIN_HOUR = 1;
-	private final static int MAX_HOUR = 24;
 
 	public ChineseHourSignParametersValidator(List<String> parameters) {
 		super(parameters);
@@ -25,18 +24,8 @@ public class ChineseHourSignParametersValidator extends CommandParametersValidat
 	@Override
 	public boolean validateParameters() throws InvalidParameterException {
 		String parameter = parameters.get(PARAMATER_INDEX);
-		try {
-			int hour = Integer.parseInt(parameter);
-			if (hour < MIN_HOUR || hour > MAX_HOUR) {
-				throw new InvalidParameterException(
-						parameter + " is an invalid value for an hour. Please, enter an integer between 1 and 24");
-			}
-		} catch (NumberFormatException numberFormatException) {
-			throw new InvalidParameterException(
-					parameter + " is an invalid value for an hour. Please, enter an integer");
-		}
-
-		return true;
+		HourParameterValidationHelper hourValidationHelper = new HourParameterValidationHelper();
+		return hourValidationHelper.validateHourParameter(parameter);
 	}
 
 }
