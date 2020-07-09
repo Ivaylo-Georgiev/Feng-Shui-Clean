bded7742103c5ce5128a66987996bef905910ef1
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-09 19:40:37 +0300

    extract validation helpers in KuaNumberParametersValidator as members

--- src/main/java/fmi/feng/shui/command/validation/KuaNumberParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/KuaNumberParametersValidator.java
@@ -5,6 +5,8 @@ import java.util.List;
 import fmi.feng.shui.command.exceptions.InvalidParameterException;
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 import fmi.feng.shui.command.validation.helpers.GenderParameterValidationHelper;
+import fmi.feng.shui.command.validation.helpers.ParameterTypeValidationHelper;
+import fmi.feng.shui.command.validation.helpers.ParametersCountValidationHelper;
 import fmi.feng.shui.command.validation.helpers.TwoParametersValidationHelper;
 import fmi.feng.shui.command.validation.helpers.YearParameterValidationHelper;
 
@@ -13,24 +15,31 @@ public class KuaNumberParametersValidator extends CommandParametersValidator {
 	private final static int YEAR_PARAMETER_INDEX = 0;
 	private final static int GENDER_PARAMETER_INDEX = 1;
 
+	private ParametersCountValidationHelper parametersCountValidationHelper;
+	private ParameterTypeValidationHelper yearValidationHelper;
+	private ParameterTypeValidationHelper genderValidationHelper;
+
 	public KuaNumberParametersValidator(List<String> parameters) {
 		super(parameters);
+		this.parametersCountValidationHelper = new TwoParametersValidationHelper();
+		this.yearValidationHelper = new YearParameterValidationHelper();
+		this.genderValidationHelper = new GenderParameterValidationHelper();
 	}
 
 	@Override
 	public boolean validateParametersCount() throws InvalidParametersCountException {
-		TwoParametersValidationHelper twoParametersTemplate = new TwoParametersValidationHelper();
-		return twoParametersTemplate.validateParametersCount(parameters);
+		boolean isParametersCountValid = parametersCountValidationHelper.validateParametersCount(parameters);
+		return isParametersCountValid;
 	}
 
 	@Override
 	public boolean validateParameters() throws InvalidParameterException {
 		String yearParameter = parameters.get(YEAR_PARAMETER_INDEX);
-		YearParameterValidationHelper yearValidationHelper = new YearParameterValidationHelper();
-		if (yearValidationHelper.validateYearParameter(yearParameter)) {
+		boolean isYearParameterValid = yearValidationHelper.validateParameter(yearParameter);
+		if (isYearParameterValid) {
 			String genderParameter = parameters.get(GENDER_PARAMETER_INDEX);
-			GenderParameterValidationHelper genderValidationHelper = new GenderParameterValidationHelper();
-			return genderValidationHelper.validateGenderParameter(genderParameter);
+			boolean isGenderParameterValid = genderValidationHelper.validateParameter(genderParameter);
+			return isGenderParameterValid;
 		}
 		return false;
 	}
