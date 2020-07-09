5c48d342649754fc0773e6133d6db7295a5fb869
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-09 19:39:59 +0300

    extract validation helpers in ChineseHourSignParametersValidator as members

--- src/main/java/fmi/feng/shui/command/validation/ChineseHourSignParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/ChineseHourSignParametersValidator.java
@@ -6,26 +6,33 @@ import fmi.feng.shui.command.exceptions.InvalidParameterException;
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 import fmi.feng.shui.command.validation.helpers.HourParameterValidationHelper;
 import fmi.feng.shui.command.validation.helpers.OneParameterValidationHelper;
+import fmi.feng.shui.command.validation.helpers.ParameterTypeValidationHelper;
+import fmi.feng.shui.command.validation.helpers.ParametersCountValidationHelper;
 
 public class ChineseHourSignParametersValidator extends CommandParametersValidator {
 
 	private final static int PARAMATER_INDEX = 0;
 
+	private ParametersCountValidationHelper parametersCountValidationHelper;
+	private ParameterTypeValidationHelper hourValidationHelper;
+
 	public ChineseHourSignParametersValidator(List<String> parameters) {
 		super(parameters);
+		this.parametersCountValidationHelper = new OneParameterValidationHelper();
+		this.hourValidationHelper = new HourParameterValidationHelper();
 	}
 
 	@Override
 	public boolean validateParametersCount() throws InvalidParametersCountException {
-		OneParameterValidationHelper oneParameterTemplate = new OneParameterValidationHelper();
-		return oneParameterTemplate.validateParametersCount(parameters);
+		boolean isParametersCountValid = parametersCountValidationHelper.validateParametersCount(parameters);
+		return isParametersCountValid;
 	}
 
 	@Override
 	public boolean validateParameters() throws InvalidParameterException {
 		String parameter = parameters.get(PARAMATER_INDEX);
-		HourParameterValidationHelper hourValidationHelper = new HourParameterValidationHelper();
-		return hourValidationHelper.validateHourParameter(parameter);
+		boolean isHourParameterValid = hourValidationHelper.validateParameter(parameter);
+		return isHourParameterValid;
 	}
 
 }
