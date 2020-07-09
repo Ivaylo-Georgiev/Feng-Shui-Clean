9667a028d659b1c7a3fa68b8653037c6c69e4a37
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-09 19:35:13 +0300

    TwoParametersValidationHelper implements ParametersCountValidationHelper

--- src/main/java/fmi/feng/shui/command/validation/helpers/TwoParametersValidationHelper.java
+++ src/main/java/fmi/feng/shui/command/validation/helpers/TwoParametersValidationHelper.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 
-public class TwoParametersValidationHelper {
+public class TwoParametersValidationHelper implements ParametersCountValidationHelper {
 
 	private final static int ALLOWED_PARAMETERS_COUNT = 2;
 
+	@Override
 	public boolean validateParametersCount(List<String> actualParameters) throws InvalidParametersCountException {
 		int parametersCount = actualParameters.size();
 		if (parametersCount != ALLOWED_PARAMETERS_COUNT) {
