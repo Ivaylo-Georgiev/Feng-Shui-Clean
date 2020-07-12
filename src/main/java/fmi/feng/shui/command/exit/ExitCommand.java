512da88b034f6343d7e85120a4dbdec8cfe01d98
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:19:21 +0300

    add class level javadoc to ExitCommand.java

--- src/main/java/fmi/feng/shui/command/exit/ExitCommand.java
+++ src/main/java/fmi/feng/shui/command/exit/ExitCommand.java
@@ -2,6 +2,12 @@ package fmi.feng.shui.command.exit;
 
 import fmi.feng.shui.command.FengShuiCommand;
 
+/**
+ * A Feng Shui command, that terminates the execution of the program.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class ExitCommand implements FengShuiCommand {
 
 	private final static String EXIT = "EXIT";
