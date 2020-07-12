0266b5a1fd4cc804036693f0e0348fb10a747f2e
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:21:57 +0300

    add class level javadoc to ChineseHourSignCommand.java

--- src/main/java/fmi/feng/shui/command/signs/ChineseHourSignCommand.java
+++ src/main/java/fmi/feng/shui/command/signs/ChineseHourSignCommand.java
@@ -2,10 +2,17 @@ package fmi.feng.shui.command.signs;
 
 import fmi.feng.shui.command.FengShuiCommand;
 
+/**
+ * A Feng Shui command, that calculates the Chinese animal sign, corresponding
+ * to an hour.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class ChineseHourSignCommand implements FengShuiCommand {
-	
+
 	private int hour;
-	
+
 	public ChineseHourSignCommand(int hour) {
 		this.hour = hour;
 	}
