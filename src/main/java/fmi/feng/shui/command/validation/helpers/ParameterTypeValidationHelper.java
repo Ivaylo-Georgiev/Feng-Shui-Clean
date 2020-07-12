76c01f046689ff5a66d1c7ccb48e68d1dc287ead
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:27:46 +0300

    add class level javadoc to ParameterTypeValidationHelper.java

--- src/main/java/fmi/feng/shui/command/validation/helpers/ParameterTypeValidationHelper.java
+++ src/main/java/fmi/feng/shui/command/validation/helpers/ParameterTypeValidationHelper.java
@@ -2,6 +2,12 @@ package fmi.feng.shui.command.validation.helpers;
 
 import fmi.feng.shui.command.exceptions.InvalidParameterException;
 
+/**
+ * Helps to validate the exact type of parameter, passed to a Feng Shui command.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 @FunctionalInterface
 public interface ParameterTypeValidationHelper {
 
