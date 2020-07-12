46143f52bdce5bcbc88a24cefa14f2db4a273cc9
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:26:57 +0300

    add class level javadoc to HourParameterValidationHelper.java

--- src/main/java/fmi/feng/shui/command/validation/helpers/HourParameterValidationHelper.java
+++ src/main/java/fmi/feng/shui/command/validation/helpers/HourParameterValidationHelper.java
@@ -2,6 +2,13 @@ package fmi.feng.shui.command.validation.helpers;
 
 import fmi.feng.shui.command.exceptions.InvalidParameterException;
 
+/**
+ * Helps to validate an hour parameter, passed to a Feng Shui command. An hour
+ * parameter is considered valid, if it is an integer between 1 and 24.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class HourParameterValidationHelper implements ParameterTypeValidationHelper {
 
 	private final static int MIN_HOUR = 1;
