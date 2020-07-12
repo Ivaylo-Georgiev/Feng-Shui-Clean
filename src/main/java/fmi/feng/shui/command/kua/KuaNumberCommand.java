db4ee34ed344501cd0d5220e6fc05884c5545dea
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 19:46:21 +0300

    add javadoc on method and member level for KuaNumberCommand.java

--- src/main/java/fmi/feng/shui/command/kua/KuaNumberCommand.java
+++ src/main/java/fmi/feng/shui/command/kua/KuaNumberCommand.java
@@ -10,17 +10,43 @@ import fmi.feng.shui.command.FengShuiCommand;
  */
 public class KuaNumberCommand implements FengShuiCommand {
 
-	// Some schools of Feng Shui do not use the Kua Number 5
+	/**
+	 * Some schools of Feng Shui do not use the Kua Number 5
+	 */
 	private final static int KUA_FIVE = 5;
-	// Females replace it with Kua 8
+
+	/**
+	 * Females replace KUA 5 with KUA 8
+	 */
 	private final static int KUA_EIGHT = 8;
-	// Males replace it with Kua 2
+
+	/**
+	 * Males replace KUA 5 with KUA 2
+	 */
 	private final static int KUA_TWO = 2;
 
+	/**
+	 * To calculate the KUA number of a female, add 4 to the reduced sum of the last
+	 * two digits of the birth year
+	 */
 	private final static int FEMALE_KUA_ADDEND = 4;
+
+	/**
+	 * To calculate the KUA number of a male, subtract the reduced sum of the last
+	 * two digits of the birth year from 11
+	 */
 	private final static int MALE_KUA_MINUEND = 11;
 
+	/**
+	 * Two parameters are needed to calculate the KUA number of a person. The first
+	 * is birth year.
+	 */
 	private int year;
+
+	/**
+	 * * Two parameters are needed to calculate the KUA number of a person. The
+	 * first is gender.
+	 */
 	private Gender gender;
 
 	public KuaNumberCommand(int year, Gender gender) {
@@ -28,6 +54,12 @@ public class KuaNumberCommand implements FengShuiCommand {
 		this.gender = gender;
 	}
 
+	/**
+	 * Calculates the KUA number of a person with the specified birth year and
+	 * gender
+	 * 
+	 * @return String The string representation of the calculated KUA number
+	 */
 	@Override
 	public String execute() {
 		if (this.gender == Gender.MALE) {
@@ -36,6 +68,11 @@ public class KuaNumberCommand implements FengShuiCommand {
 		return Integer.toString(calculateFemaleKuaNumber());
 	}
 
+	/**
+	 * Calculates the KUA number of a male with the specified birth year
+	 * 
+	 * @return int The integer representation of the calculated KUA number
+	 */
 	private int calculateMaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.year);
 		int kuaNumber = MALE_KUA_MINUEND - reducedYear;
@@ -43,6 +80,11 @@ public class KuaNumberCommand implements FengShuiCommand {
 		return (kuaNumber == KUA_FIVE) ? KUA_TWO : kuaNumber;
 	}
 
+	/**
+	 * Calculates the KUA number of a female with the specified birth year
+	 * 
+	 * @return int The integer representation of the calculated KUA number
+	 */
 	private int calculateFemaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.year);
 		int kuaNumber = FEMALE_KUA_ADDEND + reducedYear;
@@ -50,6 +92,13 @@ public class KuaNumberCommand implements FengShuiCommand {
 		return (kuaNumber == KUA_FIVE) ? KUA_EIGHT : kuaNumber;
 	}
 
+	/**
+	 * Represents in integer as the sum of it's digits, until it is reduced to a
+	 * single digit
+	 * 
+	 * @param number The number, that needs to be reduced to a single digit
+	 * @return int The number, reduced to a single digit
+	 */
 	private int reduceToSingleDigit(int number) {
 		int sum = 0;
 
