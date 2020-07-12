14d18a9e61b4cd847adc58f99fbcd2f8de4e0f5a
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 21:40:47 +0300

    add javadoc on method and members level for ParametersCountValidationHelper.java

--- src/main/java/fmi/feng/shui/command/validation/helpers/ParametersCountValidationHelper.java
+++ src/main/java/fmi/feng/shui/command/validation/helpers/ParametersCountValidationHelper.java
@@ -2,6 +2,7 @@ package fmi.feng.shui.command.validation.helpers;
 
 import java.util.List;
 
+import fmi.feng.shui.command.exceptions.InvalidParameterException;
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 
 /**
@@ -13,6 +14,14 @@ import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 @FunctionalInterface
 public interface ParametersCountValidationHelper {
 
+	/**
+	 * Validates, that the user has specified an exact number of parameters
+	 * 
+	 * @throws InvalidParameterException If the actual parameters count, provided by
+	 *                                   the user, is invalid
+	 * @param actualParameters The parameters, specified by the user
+	 * @return boolean true, if the user has provided the exact number of parameters
+	 */
 	public boolean validateParametersCount(List<String> actualParameters) throws InvalidParametersCountException;
 
 }
