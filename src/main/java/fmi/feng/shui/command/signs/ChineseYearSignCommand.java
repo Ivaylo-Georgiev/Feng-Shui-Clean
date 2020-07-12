676efcbf881f846794cd7d4ef9b77097862d82cc
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 20:18:28 +0300

    add javadoc on method and members level for ChineseearSignCommand.java

--- src/main/java/fmi/feng/shui/command/signs/ChineseYearSignCommand.java
+++ src/main/java/fmi/feng/shui/command/signs/ChineseYearSignCommand.java
@@ -11,12 +11,24 @@ import fmi.feng.shui.command.FengShuiCommand;
  */
 public class ChineseYearSignCommand implements FengShuiCommand {
 
+	/**
+	 * Only one parameter is needed to calculate the Chinese year animal sign - a
+	 * year.
+	 */
 	private int year;
 
+	/**
+	 * Constructs a Chinese year sign Feng Shui command for a specified year
+	 */
 	public ChineseYearSignCommand(int year) {
 		this.year = year;
 	}
 
+	/**
+	 * Calculate the Chinese animal sign, corresponding to the specified year
+	 * 
+	 * @return String The Chinese year animal sign
+	 */
 	@Override
 	public String execute() {
 		AnimalSign animalSign = AnimalSign.mapYearToSign(this.year);
