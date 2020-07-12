f1c4a7d0a3a82a6b455f73b2f43d07473196d840
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:26:07 +0300

    add class level javadoc to SecretFriendParametersValidator.java

--- src/main/java/fmi/feng/shui/command/validation/SecretFriendParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/SecretFriendParametersValidator.java
@@ -9,6 +9,13 @@ import fmi.feng.shui.command.validation.helpers.ParameterTypeValidationHelper;
 import fmi.feng.shui.command.validation.helpers.ParametersCountValidationHelper;
 import fmi.feng.shui.command.validation.helpers.YearParameterValidationHelper;
 
+/**
+ * Validates the parameters, passed to the Feng Shui command, that finds the
+ * secret friend of a person.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class SecretFriendParametersValidator extends CommandParametersValidator {
 
 	private final static int PARAMATER_INDEX = 0;
