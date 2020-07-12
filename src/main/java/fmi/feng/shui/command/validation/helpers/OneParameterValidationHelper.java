fc5ae46a53a2aa300679afa24d26e527af70927d
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 21:40:13 +0300

    add javadoc on method and members level for OneParameterValidationHelper.java

--- src/main/java/fmi/feng/shui/command/validation/helpers/OneParameterValidationHelper.java
+++ src/main/java/fmi/feng/shui/command/validation/helpers/OneParameterValidationHelper.java
@@ -2,6 +2,7 @@ package fmi.feng.shui.command.validation.helpers;
 
 import java.util.List;
 
+import fmi.feng.shui.command.exceptions.InvalidParameterException;
 import fmi.feng.shui.command.exceptions.InvalidParametersCountException;
 
 /**
@@ -15,6 +16,13 @@ public class OneParameterValidationHelper implements ParametersCountValidationHe
 
 	private final static int ALLOWED_PARAMETERS_COUNT = 1;
 
+	/**
+	 * Validates, that the user has specified exactly one parameter
+	 * 
+	 * @throws InvalidParameterException If less or more than 1 parameter is passed
+	 * @param actualParameters The parameters, specified by the user
+	 * @return boolean true, if the user has provided exactly one parameter
+	 */
 	@Override
 	public boolean validateParametersCount(List<String> actualParameters) throws InvalidParametersCountException {
 		int parametersCount = actualParameters.size();
