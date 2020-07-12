31d5f56fd1758ef4cd4e566eeffd40b1bccaed87
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:19:36 +0300

    add class level javadoc to Gender.java

--- src/main/java/fmi/feng/shui/command/kua/Gender.java
+++ src/main/java/fmi/feng/shui/command/kua/Gender.java
@@ -1,5 +1,12 @@
 package fmi.feng.shui.command.kua;
 
+/**
+ * Specifies the supported genders, needed for the execution of several Feng
+ * Shui commands.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public enum Gender {
 
 	MALE, FEMALE;
