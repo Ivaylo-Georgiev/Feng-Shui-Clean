d92426062cadb158c3e19687e671a927e020556f
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 20:10:40 +0300

    add javadoc on method and member level for ChineseHourSignCommand.java

--- src/main/java/fmi/feng/shui/command/signs/ChineseHourSignCommand.java
+++ src/main/java/fmi/feng/shui/command/signs/ChineseHourSignCommand.java
@@ -11,12 +11,22 @@ import fmi.feng.shui.command.FengShuiCommand;
  */
 public class ChineseHourSignCommand implements FengShuiCommand {
 
+	/**
+	 * Only one parameter is needed to calculate the Chinese hour animal sign - an
+	 * hour.
+	 */
 	private int hour;
 
+	/**
+	 * Constructs a Chinese hour sign Feng Shui command for a specified hour
+	 */
 	public ChineseHourSignCommand(int hour) {
 		this.hour = hour;
 	}
 
+	/**
+	 * Calculate the Chinese animal sign, corresponding to the specified hour
+	 */
 	@Override
 	public String execute() {
 		AnimalSign animalSign = AnimalSign.mapHourToSign(this.hour);
