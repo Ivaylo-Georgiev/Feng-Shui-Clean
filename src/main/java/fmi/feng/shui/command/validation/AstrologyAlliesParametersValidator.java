fd30689a27f68e4e2cfa695e3e6060f68c6595f4
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:23:11 +0300

    add class level javadoc to AstrologyAlliesParametersValidator.java

--- src/main/java/fmi/feng/shui/command/validation/AstrologyAlliesParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/AstrologyAlliesParametersValidator.java
@@ -9,6 +9,13 @@ import fmi.feng.shui.command.validation.helpers.ParameterTypeValidationHelper;
 import fmi.feng.shui.command.validation.helpers.ParametersCountValidationHelper;
 import fmi.feng.shui.command.validation.helpers.YearParameterValidationHelper;
 
+/**
+ * Validates the parameters, passed to the Feng Shui command, that finds the
+ * astrology allies of a person.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class AstrologyAlliesParametersValidator extends CommandParametersValidator {
 
 	private final static int PARAMATER_INDEX = 0;
