defe45d013ad6a032bd7c29736eb9a9b7f33a057
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:17:02 +0300

    add class level javadoc to Command.java

--- src/main/java/fmi/feng/shui/command/Command.java
+++ src/main/java/fmi/feng/shui/command/Command.java
@@ -3,6 +3,12 @@ package fmi.feng.shui.command;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains the supported commands, that can be executed by the program.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public enum Command {
 
 	EXIT, CHINESE_YEAR_SIGN, CHINESE_HOUR_SIGN, KUA_NUMBER, SECRET_FRIEND, ASTROLOGY_ALLIES;
