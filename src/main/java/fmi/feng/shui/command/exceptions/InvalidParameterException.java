957b08927bf43b250b5ff43a6033de5a25365f01
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:18:35 +0300

    add class level javadoc to InvalidParameterException.java

--- src/main/java/fmi/feng/shui/command/exceptions/InvalidParameterException.java
+++ src/main/java/fmi/feng/shui/command/exceptions/InvalidParameterException.java
@@ -1,5 +1,12 @@
 package fmi.feng.shui.command.exceptions;
 
+/**
+ * Signals that the command, specified by the user, is supported and the number
+ * of parameters is correct, but one of the parameters is invalid.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class InvalidParameterException extends Exception {
 
 	private static final long serialVersionUID = -6533917251099783877L;
