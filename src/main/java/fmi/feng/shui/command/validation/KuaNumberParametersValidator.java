6a76b138c9e1b9992eef25d8c198a8182a983cf6
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 21:18:47 +0300

    add javadoc on method and members level for KuaNumberParametersValidator.java

--- src/main/java/fmi/feng/shui/command/validation/KuaNumberParametersValidator.java
+++ src/main/java/fmi/feng/shui/command/validation/KuaNumberParametersValidator.java
@@ -19,13 +19,36 @@ import fmi.feng.shui.command.validation.helpers.YearParameterValidationHelper;
  */
 public class KuaNumberParametersValidator extends CommandParametersValidator {
 
+	/**
+	 * The index of the year parameter, needed for the KUA number Feng Shui command
+	 */
 	private final static int YEAR_PARAMETER_INDEX = 0;
+
+	/**
+	 * The index of the gender parameter, needed for the KUA number Feng Shui
+	 * command
+	 */
 	private final static int GENDER_PARAMETER_INDEX = 1;
 
+	/**
+	 * Helps to validate that the right number of parameters is provided by the user
+	 */
 	private ParametersCountValidationHelper parametersCountValidationHelper;
+
+	/**
+	 * Helps to validate, that the year parameter is correct
+	 */
 	private ParameterTypeValidationHelper yearValidationHelper;
+
+	/**
+	 * Helps to validate, that the gender parameter is correct
+	 */
 	private ParameterTypeValidationHelper genderValidationHelper;
 
+	/**
+	 * Constructs a validator for the KUA number Feng Shui command with the raw
+	 * parameters, specified by the user
+	 */
 	public KuaNumberParametersValidator(List<String> parameters) {
 		super(parameters);
 		this.parametersCountValidationHelper = new TwoParametersValidationHelper();
@@ -33,12 +56,27 @@ public class KuaNumberParametersValidator extends CommandParametersValidator {
 		this.genderValidationHelper = new GenderParameterValidationHelper();
 	}
 
+	/**
+	 * Verify that exactly two parameters are passed to the command
+	 * 
+	 * @throws InvalidParameterException If more or less than two parameters are
+	 *                                   passed to the command
+	 * @return boolean true, if two parameters are passed to the command
+	 */
 	@Override
 	public boolean validateParametersCount() throws InvalidParametersCountException {
 		boolean isParametersCountValid = parametersCountValidationHelper.validateParametersCount(parameters);
 		return isParametersCountValid;
 	}
 
+	/**
+	 * Verify that the provided year parameter is a positive integer and the gender
+	 * is valid
+	 * 
+	 * @throws InvalidParameterException If the year parameter, is not a positive
+	 *                                   integer, or the gender is invalid
+	 * @return boolean true, if the parameters are valid
+	 */
 	@Override
 	public boolean validateParameters() throws InvalidParameterException {
 		String yearParameter = parameters.get(YEAR_PARAMETER_INDEX);
