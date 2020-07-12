e3d5a36362467bd3acd0d7474d9830c80a3b0e27
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2020-07-12 17:22:24 +0300

    add class level javadoc to ChineseYearSignCommand.java

--- src/main/java/fmi/feng/shui/command/signs/ChineseYearSignCommand.java
+++ src/main/java/fmi/feng/shui/command/signs/ChineseYearSignCommand.java
@@ -2,6 +2,13 @@ package fmi.feng.shui.command.signs;
 
 import fmi.feng.shui.command.FengShuiCommand;
 
+/**
+ * A Feng Shui command, that calculates the Chinese animal sign, corresponding
+ * to a year.
+ * 
+ *
+ * @author Ivaylo Georgiev
+ */
 public class ChineseYearSignCommand implements FengShuiCommand {
 
 	private int year;
